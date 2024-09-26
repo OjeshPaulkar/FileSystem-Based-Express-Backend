cf560b9093305c266df09f05f72f942be186502f
Ojesh <ojeshpaulkar@gmail.com>
2024-09-26 22:02:45 +0530

    added updateTodo API

--- index.js
+++ index.js
@@ -108,3 +108,36 @@ try {
 app.listen(port, () => {
     console.log(`App is Live on Port ${port}`);
 })
+
+
+app.put("/updateTodo", auth, async (req,res) => {
+    try {
+        const todoId = req.body._id;
+        const todoStatus = req.body.status;
+        const { description } = req.body;
+        const userId = req.userId;
+        console.log(userId)
+    
+        const todo = await TodoModel.findOne({
+            _id: todoId,
+        })
+    
+        if(!todo) {
+           return res.status(403).json({message: "Incorrect Todo ID"})
+        }
+        const dbUserId = todo.userId.toString();
+        console.log(dbUserId);
+
+        if (userId===dbUserId) {
+            todo.description = description;
+            todo.status = todoStatus;
+
+            await todo.save();
+            return res.status(200).json({message: "Todo Updated Sucessfully"});
+        }else{
+            return res.status(403).json({message : "Invalid user, Cannot update Todo"});
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    } 
+})
